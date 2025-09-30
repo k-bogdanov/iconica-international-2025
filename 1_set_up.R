89c9331c3f7fc5e5e804ec229e39f463b1d47792
K. B. <151694873+k-bogdanov@users.noreply.github.com>
2025-09-30 14:39:13 +1300

    Add files via upload

---
+++ 1_set_up.R
@@ -0,0 +1,67 @@
+# load libraries
+library(ggplot2)
+library(phyloseq)
+library(dplyr)
+library(microViz)
+library(ranacapa)
+library(tidyr)
+library(Rmisc)
+library(tidyverse)
+library(vegan)
+library(grid)
+library(ggrepel)
+library(ggforce)
+library(ggpubr)
+library(ggfortify)
+library(rstatix)
+library(microbiome)
+library(scales)
+library(speedyseq)
+library(phylosmith)
+library(fantaxtic)
+library(reshape2)
+library(ggsci)
+library(microbiomeutilities)
+library(ggtern)
+library(cowplot)
+library(patchwork)
+library(ggcorrplot)
+library(ggh4x)
+library(ggdendro)
+library(metagMisc)
+
+# theme
+my_theme <- theme(axis.title.y = element_text(size = 12, face = "bold", vjust = 3),
+                  axis.title.x = element_text(size = 12, face = "bold", vjust = -1),
+                  panel.background = element_rect(fill = "white", colour = "grey90"),
+                  strip.text.x = element_text(size = 12, colour = 'black'),
+                  legend.key.width = unit(0.5, "cm"),
+                  legend.key.height = unit(0.5, "cm"),
+                  axis.text = element_text(color = "grey40"),
+                  axis.ticks = element_blank(),
+                  strip.text = element_text(colour = "black", size = 12),
+                  panel.grid = element_line(color = "grey90"),
+                  panel.grid.major = element_blank(),
+                  plot.margin = margin(l = 5, b = 5, r = 5, t = 5),
+                  panel.grid.minor = element_blank(),
+                  plot.title = element_text(face = "bold"),
+                  strip.background = element_rect(colour = "grey90", fill ="white"),
+                  legend.position = "right", legend.title = element_text(size = 12),
+                  legend.text = element_text(size = 12),
+                  legend.key = element_rect(fill = NA))
+
+# colours
+col1 <- "#AE445A"
+col2 <- "#4B4376"
+col3 <- "#B1C29E"
+col4 <- "#FFE6A9"
+col5 <- "#DEAA79"
+col6 <- "#447D9B"
+col7 <- "#9FC87E"
+col8 <- "#FF9898"
+col9 <- "#9B7EBD"
+col10 <- "#309898"
+
+# import phyloseq objects
+all_16s_phyloseq <- readRDS("~/iconica_all_16s_ps.rds")
+all_ITS_phyloseq <- readRDS("~/iconica_all_its_ps.rds")
